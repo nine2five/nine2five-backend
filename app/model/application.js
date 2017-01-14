bb56c053e6b4b549adfc0debac11668216f656ca
Lee Broxson <broxsonl@gmail.com>
2017-01-14 14:45:32 -0800

    Created contact model.

--- app/model/application.js
+++ app/model/application.js
@@ -8,16 +8,20 @@ function query(client) {
     `CREATE TABLE IF NOT EXISTS
     applications(
       id SERIAL PRIMARY KEY,
+      user_id INTEGER
       FOREIGN KEY(user_id) REFERENCES users(id),
+      status_id INTEGER
       FOREIGN KEY(status_id) REFERENCES statuses(id),
       company VARCHAR(255),
       position VARCHAR(255),
       date_applied DATE,
+      contact_id INTEGER
       FOREIGN KEY(contact_id) REFERENCES contacts(id),
       link_posting TEXT,
       last_communication DATE,
       interview_date DATE,
       additional_info TEXT,
+      offer_id INTEGER
       FOREIGN KEY(offer_id) REFERENCES offers(id)
     )`
   );
---
+++ app/model/contact.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import pg from 'pg';
+
+function query(client) {
+
+  return client.query(
+    `CREATE TABLE IF NOT EXISTS
+    contacts(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255),
+      email VARCHAR(255),
+      phone VARCHAR(20),
+      title VARCHAR(255)
+    )`
+  );
+}
+
+export default query;
