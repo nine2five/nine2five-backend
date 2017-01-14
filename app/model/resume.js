df4e18c5f5b62f7d401db921610695825b361dc3
raziyeh bazargan <r.bazargan@gmail.com>
2017-01-14 14:33:50 -0800

    created category model! done

--- app/model/resume.js
+++ app/model/resume.js
@@ -6,10 +6,10 @@ function query(client) {
 
   return client.query(
     `CREATE TABLE IF NOT EXISTS
-    resume(
+    resumes(
       id SERIAL PRIMARY KEY,
-      FOREIGN KEY(user_id) REFERENCES user(id),
-      FOREIGN KEY(category_id) REFERENCES category(id),
+      FOREIGN KEY(user_id) REFERENCES users(id),
+      FOREIGN KEY(category_id) REFERENCES categories(id),
       title VARCHAR(50) NOT NULL,
       fileType VARCHAR(40) NOT NULL,
       fileSrc VARCHAR(40) NOT NULL,
