adcd1d2e65abe2ef7da656ad175f95ad4fe1e6ea
Stefanie Hansen <stefuhnee@gmail.com>
2017-01-18 20:21:42 -0800

    deleted commented out code

--- app/model/user.js
+++ app/model/user.js
@@ -13,19 +13,3 @@ export function User(sequelize){
     },
   });
 }
-
-// import pg from 'pg';
-//
-// function query(client) {
-//   return client.query(
-//     `CREATE TABLE IF NOT EXISTS
-//     users(
-//       id SERIAL PRIMARY KEY,
-//       password VARCHAR(255) NOT NULL,
-//       email VARCHAR(255) NOT NULL,
-//       is_verified BOOLEAN DEFAULT false
-//     )`
-//   );
-// }
-//
-// export default query;
