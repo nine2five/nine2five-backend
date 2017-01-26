7f7b3db83312385fb029e73e2b64381e998d16b9
Stefanie Hansen <stefuhnee@gmail.com>
2017-01-25 18:39:08 -0800

    deleted pg from dependencies and some unnecessary lines

--- test/example-test.js
+++ test/example-test.js
@@ -2,19 +2,12 @@
 
 const Sequelize = require('sequelize');
 const expect = require('chai').expect;
-const pg = require('pg');
 const dotenv = require('dotenv');
 dotenv.load({path: `${__dirname}/.testenv`});
 
 const dbPopulate = require('../build/lib/db');
-const dbSetup = require('./lib/database-setup');
 const serverCtrl = require('./lib/server-ctrl');
 const server = require('../server');
-
-const dbName = 'nine2fivetest';
-const host = 'localhost';
-const port = '5432';
-
 
 describe('Sample test for creating DBs', function() {
 
