71c737acb0c1a5d10a9ac30bb939823684f3bc79
Stefanie Hansen <stefuhnee@gmail.com>
2017-02-08 19:53:22 -0800

    added yarn

--- app/route/auth-router.js
+++ app/route/auth-router.js
@@ -6,10 +6,39 @@ const jsonParser = require('body-parser').json();
 const createError = require('http-errors');
 
 const User = require('../lib/db-connection').user;
+const basicAuth = require('../lib/basic-auth-middleware');
+const bearerAuth = require('../lib/bearer-auth-middleware');
 
 const authRouter = module.exports = Router();
 
 authRouter.post('/api/signup', jsonParser, function(req, res, next){
+  debug('/api/signup route');
+
+  let password = req.body.password;
+  delete req.body.password;
+
+  if (!password)
+    return next(createError(400, 'Password required'));
+  if (password.length < 8)
+    return next(createError(400, 'Password should be at least 8 characters long'));
+
+  User.generatePasswordHash(password)
+  .then(hash => {
+    return User.create({
+      email : req.body.email,
+      password: hash,
+    });
+  })
+  .then(user => {
+    console.log(user, 'user');
+    return user.generateToken();
+  })
+  .then(token => res.send(token))
+  .catch(next);
+});
+
+authRouter.get('/api/login', bearerAuth, function(req, res, next){
+  debug('/api/login route');
   User.create({
     email : req.body.email,
     password: req.body.password,
