e26bbbdc336e4097be57f258326b2deb2a2f1bd1
Stefanie Hansen <stefuhnee@gmail.com>
2017-02-08 18:30:29 -0800

    added defaults and allow nulls

--- app/model/user.js
+++ app/model/user.js
@@ -2,12 +2,15 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define('user', {
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     isVerified: {
       type: DataTypes.BOOLEAN,
+      defaultValue : false,
     },
   });
 };
