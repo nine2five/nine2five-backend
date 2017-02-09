ecbedc97ad5bfbe3301f09742cdec8292b379ecf
meeksfred <maxfred0025@gmail.com>
2017-02-08 19:28:33 -0800

    moved directories into src for babel transpiling, changed file paths

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const morgan = require('morgan');
 const express = require('express');
 
-const sequelize = require('./lib/db-connection').sequelize;
-const authRouter = require('./route/auth-router');
+const sequelize = require('./src/lib/db-connection').sequelize;
+const authRouter = require('./src/route/auth-router');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
